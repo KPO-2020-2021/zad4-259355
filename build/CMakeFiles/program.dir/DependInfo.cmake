3c142fb65e39bb9f2e998233f03057a754063683
Adam Modlich <259355@student.pwr.edu.pl>
2021-05-10 20:43:45 +0200

    first commit 3D

---
+++ build/CMakeFiles/program.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/adam/Programowanie_Obiektowe/zad4-259355/src/Prostokat.cpp" "/home/adam/Programowanie_Obiektowe/zad4-259355/build/CMakeFiles/program.dir/src/Prostokat.cpp.o"
+  "/home/adam/Programowanie_Obiektowe/zad4-259355/src/example.cpp" "/home/adam/Programowanie_Obiektowe/zad4-259355/build/CMakeFiles/program.dir/src/example.cpp.o"
+  "/home/adam/Programowanie_Obiektowe/zad4-259355/src/lacze_do_gnuplota.cpp" "/home/adam/Programowanie_Obiektowe/zad4-259355/build/CMakeFiles/program.dir/src/lacze_do_gnuplota.cpp.o"
+  "/home/adam/Programowanie_Obiektowe/zad4-259355/src/matrix3.cpp" "/home/adam/Programowanie_Obiektowe/zad4-259355/build/CMakeFiles/program.dir/src/matrix3.cpp.o"
+  "/home/adam/Programowanie_Obiektowe/zad4-259355/src/vector3.cpp" "/home/adam/Programowanie_Obiektowe/zad4-259355/build/CMakeFiles/program.dir/src/vector3.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ENABLE_DOCTEST_IN_LIBRARY"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../include"
+  "../external/doctest/doctest"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
